09f1c7900574ff4354f7c5a0283f098592f1a97a
Takaedza <tmandie@live.com>
2024-10-05 20:38:32 +0200

    updating Get Endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 // Define your routes here
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
   res.send('Hello World!');
-});
+}); */
+
+router.use('/users', require('./users'));
 
 // Export the router
 module.exports = router;
