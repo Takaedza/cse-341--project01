bf39e4345ca10993238d06420a6d9908ff86e812
Takaedza <tmandie@live.com>
2024-10-06 00:36:26 +0200

    week 1 complete

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 // Define your routes here
-/*router.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.send('Hello World!');
-}); */
+});
 
 router.use('/users', require('./users'));
 
