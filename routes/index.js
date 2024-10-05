438d0b404b6b8c1f13b73e43fc3f172db6d1f245
Takaedza <tmandie@live.com>
2024-10-05 18:48:33 +0200

    initial check in for project 1 lesson 1

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+// ./routes/index.js
+const express = require('express');
+const router = express.Router();
+
+// Define your routes here
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Export the router
+module.exports = router;
