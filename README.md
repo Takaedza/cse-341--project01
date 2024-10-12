8538b16a80e0290754840cc5fdefd71fc31801dd
Takaedza <tmandie@live.com>
2024-10-12 17:44:44 +0200

    installed swagger

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# cse-341--project01
+# cse-341--project1
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', require('./swagger'));
+
 // Define your routes here
 router.get('/', (req, res) => {
+  //#swagger.tags=['Hello World']
   res.send('Hello World!');
 });
 
